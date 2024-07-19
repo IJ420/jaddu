9200aa287a2239d28fc5632aa6918a66532c029c
IJ420 <indrajitjana1717@gmail.com>
2024-07-19 13:53:41 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/IJ420/jaddu /jaddu
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /jaddu
 fi
-cd /Advance-File-Share-Bot
+cd /jaddu
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
